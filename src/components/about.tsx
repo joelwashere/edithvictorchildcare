82712898f3599c95ebb0a1f7e419259a8707a9e1
joel <joel@reboundstudios.com>
2023-05-05 14:10:18 -0400

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,7 +4,7 @@ export const About: FC = () => {
 
   return (
     <section className="flex flex-col text-[#313639] bg-[#f2f2f2] pt-10 pb-20 px-8 text-xl lg:2xl">
-      <div className="flex flex-row flex-auto mx-auto">
+      <div className="flex lg:flex-row flex-auto mx-auto">
         <p className="text-center max-w-5xl tracking-tight text-2xl">
           Welcome to Edith & Victor Family Childcare, where we provide high-quality childcare services for families with young children.
           Edith and Victor have a passion for creating a nurturing and stimulating environment for children.
